c1d92e938915ee85582c0af8ecbe24f23a5f9b2f
Eduardo Gutierrez <eduardo.gutierrez@grupovb.com.br>
2025-05-26 19:47:43 -0300

    test

--- lib/services/ee_builder.sh
+++ lib/services/ee_builder.sh
@@ -8,8 +8,7 @@ source "$(dirname "${BASH_SOURCE[0]}")/../utils/common.sh"
 
 # Configurações do módulo
 readonly EE_MODULE_NAME="ee_builder"
-TEMPLATES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-TEMPLATES_DIR="${TEMPLATES_DIR}/config/templates"
+readonly TEMPLATES_DIR="${PROJECT_ROOT}/config/templates"
 readonly EE_TEMP_DIR="/tmp/awx-ee-$(date +%s)"
 
 # Variáveis de configuração
